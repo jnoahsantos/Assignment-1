801895e606037e48bea6d6af17286bfcea850d6b
jnoahsantos <jamesnoah.santos27@gmail.com>
2021-10-22 23:15:11 +0800

    initial commit of master class

---
+++ Assignment 1/Assignment 1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string message = "James Noah A. Santos";
+            string title = "Assigment 1";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.OK)
+            {
+                this.Close();
+            }
+        }
+ 
+    }
+}
